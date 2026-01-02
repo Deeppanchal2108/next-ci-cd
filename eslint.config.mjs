d3f25d07e167b2ff70cb2136b44e0ff6834269a9
Deeppanchal2108 <deepanchal2290@gmail.com>
2026-01-02 17:39:53 +0530

    fixed mistake

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,17 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+  
+    rules: {
+      "no-console": "warn",           // Warn if console.log is left in code
+      "prefer-const": "error",        // Error if a variable can be a const
+      "@typescript-eslint/no-explicit-any": "error", // Force better types
+      "react/react-in-jsx-scope": "off", // Next.js doesn't need React imported
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
