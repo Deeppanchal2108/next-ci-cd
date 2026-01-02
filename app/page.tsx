6a154fe8d91b3901402cf0c0f24f5e5fa2be4c2e
Deeppanchal2108 <deepanchal2290@gmail.com>
2026-01-02 16:08:18 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      
+      <h1>Welcome to the Home Page</h1>
+      <p> TThis is just a demo repo I'm implementing ci cd </p>
+    </div>
+  )
+}
+
+export default page
